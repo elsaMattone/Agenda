527a83df6fda47e61f53faa434c6640b0fdb4f1f
emattone <elsa.mattone@etud.univ-jfc.fr>
2021-12-13 16:42:52 +0100

    Repetitive

--- src/main/java/agenda/RepetitiveEvent.java
+++ src/main/java/agenda/RepetitiveEvent.java
@@ -8,6 +8,9 @@ import java.time.temporal.ChronoUnit;
  * Description : A repetitive Event
  */
 public class RepetitiveEvent extends Event {
+    
+    private ChronoUnit frequency;
+    private List<LocalDate> listException = new ArrayList<LocalDate>();
     /**
      * Constructs a repetitive event
      *
@@ -23,8 +26,7 @@ public class RepetitiveEvent extends Event {
      */
     public RepetitiveEvent(String title, LocalDateTime start, Duration duration, ChronoUnit frequency) {
         super(title, start, duration);
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        this.frequency = frequency;
     }
 
     /**
@@ -33,8 +35,9 @@ public class RepetitiveEvent extends Event {
      * @param date the event will not occur at this date
      */
     public void addException(LocalDate date) {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        if (isInDay(date)==true){
+            listException.add(date);
+        }
     }
 
     /**
@@ -42,8 +45,7 @@ public class RepetitiveEvent extends Event {
      * @return the type of repetition
      */
     public ChronoUnit getFrequency() {
-        // TODO : implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");    
+        return frequency; 
     }
 
 }
